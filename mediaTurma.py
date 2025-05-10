085f583314d11b382dc622c136ee9772000f6bff
YukiSukiyama <rspsukiyama21@gmail.com>
2025-05-09 22:22:15 -0300

    mudanca

--- mediaTurma.py
+++ mediaTurma.py
@@ -1,6 +1,7 @@
-valor1 = float(input("digite as sua nota"))
-valor2 = float(input("digite as sua nota"))
-valor3 = float(input("digite as sua nota"))
-valor4 = float(input("digite as sua nota"))
+valor1 = float(input("digite a sua nota"))
+valor2 = float(input("digite a sua nota"))
+valor3 = float(input("digite a sua nota"))
+valor4 = float(input("digite a sua nota"))
+valor5 = float(input("digite a sua nota"))
 media = (valor1 + valor2 + valor3 + valor4)/4
 print("a sua média é:",media)

31576c697391eae507dec59e0b594ef256be0b97
YukiSukiyama <rspsukiyama21@gmail.com>
2025-05-09 21:59:14 -0300

    alteracao

--- mediaTurma.py
+++ mediaTurma.py
@@ -2,5 +2,6 @@ valor1 = float(input("digite as sua nota"))
 valor2 = float(input("digite as sua nota"))
 valor3 = float(input("digite as sua nota"))
 valor4 = float(input("digite as sua nota"))
-media = (valor1 + valor2 + valor3 + valor4)/4
+valor5 = float(input("digite as sua nota"))
+media = (valor1 + valor2 + valor3 + valor4 + valor5)/5
 print("a sua média é:",media)

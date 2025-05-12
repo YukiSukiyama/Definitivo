139f8d68199406a53054df7fae560f3fb542d9ae
Mel Isis <honeybee7415@gmail.com>
2025-05-12 14:37:55 -0300

    Update mediaTurma.py

--- mediaTurma.py
+++ mediaTurma.py
@@ -2,6 +2,5 @@ valor1 = float(input("digite a sua nota"))
 valor2 = float(input("digite a sua nota"))
 valor3 = float(input("digite a sua nota"))
 valor4 = float(input("digite a sua nota"))
-valor5 = float(input("digite a sua nota"))
-media = (valor1 + valor2 + valor3 + valor4)/5
+media = (valor1 + valor2 + valor3 + valor4)/4
 print("a sua média é:",media)
